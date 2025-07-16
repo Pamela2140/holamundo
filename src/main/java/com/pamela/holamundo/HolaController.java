81d7599c93d4b78777df08344850f7222fc34394
Pamela Flores <floreschoconpamelafaviana@gmail.com>
2025-07-16 13:32:44 -0600

    Tarea 1

---
+++ src/main/java/com/pamela/holamundo/HolaController.java
@@ -0,0 +1,13 @@
+package com.pamela.holamundo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+
+    @GetMapping("/")
+    public String hola() {
+        return "¡Hola Mundo desde Spring Boot!";
+    }
+}
